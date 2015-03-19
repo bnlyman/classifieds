793065728b2514a86b4351bd1ea227eb3bac537c
ben lyman <bnlyman@gmail.com>
2015-03-19 11:29:36 -0600

    Added users controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
-
+  
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  
+  resources :users, :only => [:show, :index]
+  get '/profile', to: 'users#profile', as: :profile
   get '/about', to: 'static_pages#about', as: :about 
 
   resources :items
